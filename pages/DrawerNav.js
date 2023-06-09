f6e4b38b0dca4058c9e07c3f88e388edc9668e70
Dev-RMV <71718060+Dev-RMV@users.noreply.github.com>
2023-06-09 07:57:26 -0300

    Diferenças entre plataformas demonstrada explicitamente
    No título da página/drawer. Sintam-se a vontade de alterar como foi feito

--- pages/DrawerNav.js
+++ pages/DrawerNav.js
@@ -1,10 +1,18 @@
 import { createDrawerNavigator } from "@react-navigation/drawer";
 import HomeScreen from "./screens/HomeScreen";
 import AddClaim from "./screens/AddClaimScreen";
+import { Platform } from "react-native";
+import { useState, useLayoutEffect } from "react";
 
 const Drawer = createDrawerNavigator();
 
 export default function DrawerNav({ companies, addClaim, userLogged }) {
+  const [plataforma, setPlataforma] = useState("");
+  useLayoutEffect(() => {
+    if (Platform.OS == "ios") return setPlataforma("IOS");
+    if (Platform.OS == "android") return setPlataforma("Android");
+    return setPlataforma("Web");
+  }, []);
   return (
     <Drawer.Navigator
       screenOptions={{
@@ -13,7 +21,7 @@ export default function DrawerNav({ companies, addClaim, userLogged }) {
         drawerStyle: { backgroundColor: "#F0F0F0" },
       }}
     >
-      <Drawer.Screen name="Início">
+      <Drawer.Screen name={`Início - ${plataforma}`}>
         {() => <HomeScreen companies={companies} />}
       </Drawer.Screen>
       <Drawer.Screen name="Adicionar Reclamação">
